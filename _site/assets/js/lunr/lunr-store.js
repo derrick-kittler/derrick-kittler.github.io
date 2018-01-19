126b36f77566070f0de31c99d874b342aa4cc25c
Derrick Kittler <dkittler@Derricks-MBP.home>
2018-01-19 12:17:22 -0500

    added post and changes for theme

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
         "url": "http://localhost:4000/jekyll/update/2018/01/18/welcome-to-jekyll.html",
+        "teaser":null},{
+        "title": "Modern Application Development: Should you skip microservices and go directly to serverless?",
+        "excerpt":"You don’t have to look further than the Gartner Hype Cycle to see the staggering pace of innovation in application architecture. Microservices, reactive programming, and container management are among the architectural models at the peak of the Cycle, making deep scrutiny a must as you consider the systems, processes, and...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/2018/01/19/Modern_App_Dev.html",
         "teaser":null}]
